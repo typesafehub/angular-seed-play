4beeddf1ffb66740ddfd9f0d62657b8c14eccbac
Christopher Hunt <huntchr@gmail.com>
2013-02-27 18:31:58 +1100

    Initial import

---
+++ project/Build.scala
@@ -0,0 +1,18 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName = "angular-seed-play"
+  val appVersion = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    "org.webjars" % "webjars-play" % "2.1.0",
+    "org.webjars" % "angularjs" % "1.0.3",
+
+    javaCore)
+
+  val main = play.Project(appName, appVersion, appDependencies).settings()
+
+}
