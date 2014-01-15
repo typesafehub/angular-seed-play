15e86e9e88bd675e901721682c5a56c84c29039a
Norbert Schultz <nosc@cgvis.de>
2014-01-15 17:49:06 +0100

    Dependency to routes

--- public/js/app.js
+++ public/js/app.js
@@ -8,12 +8,18 @@ define('angular', ['webjars!angular-locale_en-us.js'], function() {
     return angular;
 });
 
-require(['angular', './controllers', './directives', './filters', './services'],
+requirejs.config({
+    shim: {
+        'webjars!angular-route.js': [ 'webjars!angular.js' ]
+    },
+ })
+
+require(['angular', 'webjars!angular-route.js', './controllers', './directives', './filters', './services'],
   function(angular, controllers) {
 
 // Declare app level module which depends on filters, and services
 
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: controllers.MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: controllers.MyCtrl2});
