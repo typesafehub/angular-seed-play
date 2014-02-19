8252ee4e91ade1b379c590c1763a8a872d6ea382
James Ward <james@jamesward.org>
2014-02-18 20:23:05 -0700

    remove static setup option

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,13 +5,8 @@ import play.api.http.MimeTypes
 
 object Application extends Controller {
 
-  def dynamicIndex = Action {
-    Ok(views.html.DynamicIndex())
-  }
-  
-  // returns the JavaScript used to setup RequireJS for WebJars
-  def setupRequireJs = Action {
-    Ok(org.webjars.RequireJS.getSetupJavaScript(routes.WebJarAssets.at("").url)).as(MimeTypes.JAVASCRIPT)
+  def index = Action {
+    Ok(views.html.Index())
   }
   
 }
