ef888b612749399d7c844eb10d087c602c8c8ccf
Christopher Hunt <huntchr@gmail.com>
2014-05-08 15:23:01 -0700

    Changes to accommodate Play 2.3.0 and sbt-web 1.0.0

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,4 +5,10 @@ logLevel := Level.Warn
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 // Use the Play sbt plugin for Play projects
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.1")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-rjs" % "1.0.1")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-digest" % "1.0.0")
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-gzip" % "1.0.0")
--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,35 @@
-/*global define, angular */
+/*global require, requirejs */
 
 'use strict';
+
+requirejs.config({
+  paths: {
+    'angular': ['../lib/angularjs/angular'],
+    'angular-route': ['../lib/angularjs/angular-route']
+  },
+  shim: {
+    'angular': {
+      exports : 'angular'
+    },
+    'angular-route': {
+      deps: ['angular'],
+      exports : 'angular'
+    }
+  }
+});
 
 require(['angular', './controllers', './directives', './filters', './services', 'angular-route'],
   function(angular, controllers) {
 
     // Declare app level module which depends on filters, and services
-    
+
     angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute']).
       config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: controllers.MyCtrl1});
         $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: controllers.MyCtrl2});
         $routeProvider.otherwise({redirectTo: '/view1'});
-    }]);
-      
+      }]);
+
     angular.bootstrap(document, ['myApp']);
 
 });
